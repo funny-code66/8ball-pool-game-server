c9b30ad07916f5b1b07b1d778f62943afa1560cc
stewart113ryerson <longc3505@gmail.com>
2022-06-02 04:39:13 +0800

    feat: if isNotMyTurn then disable screen.

--- rooms/ballpool.ts
+++ rooms/ballpool.ts
@@ -7,6 +7,7 @@ const BOARD_WIDTH = 3;
 class State extends Schema {
   @type("string") currentTurn: string;
   @type("string") creator: string;
+  @type("string") joiner: string;
   @type({ map: "boolean" }) players = new MapSchema<boolean>();
   @type(["number"]) board: number[] = new ArraySchema<number>(0, 0, 0, 0, 0, 0, 0, 0, 0);
   @type("string") winner: string;
@@ -17,9 +18,10 @@ export class BallPool extends Room<State> {
   maxClients = 2;
   randomMoveTimeout: Delayed;
 
-  onCreate() {
+  onCreate(options: any) {
     this.setState(new State());
-    this.onMessage("action", (client, message) => this.playerAction(client, message));
+    // this.onMessage("action", (client, message) => this.playerAction(client, message));
+    this.onMessage("change_turn", (client) => this.playerChangeTurn(client));
   }
 
   onJoin(client: Client) {
@@ -32,68 +34,20 @@ export class BallPool extends Room<State> {
     else if (this.state.players.size === 2) {
       // this.setAutoMoveTimeout();
       // lock this room for new users
+      this.state.joiner = client.sessionId;
       this.lock();
     }
   }
 
-  playerAction(client: Client, data: any) {
-    if (this.state.winner || this.state.draw) {
-      return false;
-    }
-
-    if (client.sessionId === this.state.currentTurn) {
-      const playerIds = Array.from(this.state.players.keys());
-
-      const index = data.x + BOARD_WIDTH * data.y;
-
-      if (this.state.board[index] === 0) {
-        const move = (client.sessionId === playerIds[0]) ? 1 : 2;
-        this.state.board[index] = move;
-
-        if (this.checkWin(data.x, data.y, move)) {
-          this.state.winner = client.sessionId;
-
-        } else if (this.checkBoardComplete()) {
-          this.state.draw = true;
-
-        } else {
-          // switch turn
-          const otherPlayerSessionId = (client.sessionId === playerIds[0]) ? playerIds[1] : playerIds[0];
-
-          this.state.currentTurn = otherPlayerSessionId;
-
-          this.setAutoMoveTimeout();
-        }
-
-      }
-    }
-  }
-
-  setAutoMoveTimeout() {
-    if (this.randomMoveTimeout) {
-      this.randomMoveTimeout.clear();
-    }
-
-    this.randomMoveTimeout = this.clock.setTimeout(() => this.doRandomMove(), TURN_TIMEOUT * 1000);
+  playerChangeTurn(client: Client) {
+    this.state.currentTurn = (client.sessionId == this.state.creator) ? this.state.joiner : this.state.creator;
+    console.log(this.state.currentTurn)
   }
 
   checkBoardComplete() {
     return this.state.board
       .filter(item => item === 0)
       .length === 0;
-  }
-
-  doRandomMove() {
-    const sessionId = this.state.currentTurn;
-    for (let x = 0; x < BOARD_WIDTH; x++) {
-      for (let y = 0; y < BOARD_WIDTH; y++) {
-        const index = x + BOARD_WIDTH * y;
-        if (this.state.board[index] === 0) {
-          this.playerAction({ sessionId } as Client, { x, y });
-          return;
-        }
-      }
-    }
   }
 
   checkWin(x, y, move) {
